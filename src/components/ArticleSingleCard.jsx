f60a00332b0a5a6d637047708b6dd16a938ce658
flynnjim <flynnlomas@hotmail.co.uk>
2024-09-20 14:20:57 +0100

    added in error handling for non existent path, non existent article and non existent topic. also added in loading spinners for loading articles and single article

--- src/components/ArticleSingleCard.jsx
+++ src/components/ArticleSingleCard.jsx
@@ -2,6 +2,9 @@ import ArticleCard from "./ArticleCard"
 import { useParams } from "react-router-dom"
 import { useState, useEffect} from 'react'
 import { getCurrentArticle } from "../../api"
+import NotFoundPage from "./NotFoundPage"
+import  CircularProgress from "@mui/material/CircularProgress"
+import { Typography, Box } from "@mui/material"
 
 
 const ArticleSingleCard = () => {
@@ -19,6 +22,7 @@ const ArticleSingleCard = () => {
     const {article_id} = useParams()
 
     const [isPageLoading, setIsPageLoading] = useState(true)
+    const [noArticlesFound, setNoArticlesFound] = useState(false)
 
 
     useEffect(() => {
@@ -26,7 +30,12 @@ const ArticleSingleCard = () => {
 
     getCurrentArticle(article_id)
         .then(({article}) => {
+            setNoArticlesFound(false)
             setCurrentArticle(article)
+        })
+        .catch((err) => {
+            console.log(err);
+            setNoArticlesFound(true)
         })
         .finally(() => {
         setIsPageLoading(false)
@@ -34,22 +43,36 @@ const ArticleSingleCard = () => {
     }, [article_id])
 
     if(isPageLoading) {
-        return (<h1>Article is loading...</h1>)
+        return (
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <CircularProgress />
+              <Typography variant="h6">
+                Loading article, please wait...
+              </Typography>
+            </Box>
+          );
     } else {
 
  
     return (
-        <ArticleCard
-        article_img_url={currentArticle.article_img_url}
-        author={currentArticle.author}
-        comment_count={currentArticle.comment_count}
-        created_at={currentArticle.created_at}
-        title={currentArticle.title}
-        topic={currentArticle.topic}
-        article_id={currentArticle.article_id}
-        body={currentArticle.body}
-        votes={currentArticle.votes}
-        />
+        <>
+        {noArticlesFound ? (
+            <NotFoundPage/>
+        ) : (
+            
+            <ArticleCard
+            article_img_url={currentArticle.article_img_url}
+            author={currentArticle.author}
+            comment_count={currentArticle.comment_count}
+            created_at={currentArticle.created_at}
+            title={currentArticle.title}
+            topic={currentArticle.topic}
+            article_id={currentArticle.article_id}
+            body={currentArticle.body}
+            votes={currentArticle.votes}
+            />
+        )}
+        </>
     )
     }
 }
