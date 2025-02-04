9bd416913b56816ee7018eafa45510cd618485ac
flynnjim <flynnlomas@hotmail.co.uk>
2025-02-04 12:03:46 +0000

    removing obsolete mui style components

--- src/components/ArticleCardActionComponent.jsx
+++ src/components/ArticleCardActionComponent.jsx
@@ -7,25 +7,23 @@ const ArticleCardActionComponent = ({
   buttonDisplay,
   openCommentForm,
 }) => {
-  const [showCommentsClicked, setShowCommentsClicked] = useState(false); 
-  const [writeCommentClicked, setWriteCommentClicked] = useState(false); 
+  const [showCommentsClicked, setShowCommentsClicked] = useState(false);
+  const [writeCommentClicked, setWriteCommentClicked] = useState(false);
 
   const linkText =
     "text-xs sm:text-sm md:text-md lg:text-lg font-semibold block rounded-lg text-center transition-transform transform hover:scale-105 text-black";
 
-  
   const getButtonStyle = (disabled, isClicked) => {
     return disabled
       ? "bg-gray-300 text-gray-500 cursor-not-allowed"
       : isClicked
-      ? "bg-fluorescent-yellow text-black" 
+      ? "bg-fluorescent-yellow text-black"
       : "bg-buttonpink text-black hover:bg-fluorescent-yellow";
   };
 
-  
   const handleWriteCommentClick = () => {
-    setWriteCommentClicked(!writeCommentClicked); 
-    openCommentForm(); 
+    setWriteCommentClicked(!writeCommentClicked);
+    openCommentForm();
   };
 
   return (
@@ -41,15 +39,21 @@ const ArticleCardActionComponent = ({
                     setShowCommentsClicked(!showCommentsClicked);
                   }}
                   disabled={isLoadingComments}
-                  className={`${linkText} ${getButtonStyle(isLoadingComments, showCommentsClicked)}`}
+                  className={`${linkText} ${getButtonStyle(
+                    isLoadingComments,
+                    showCommentsClicked
+                  )}`}
                 >
                   {buttonDisplay}
                 </button>
               </li>
               <li>
                 <button
-                  onClick={handleWriteCommentClick} 
-                  className={`${linkText} ${getButtonStyle(false, writeCommentClicked)}`}
+                  onClick={handleWriteCommentClick}
+                  className={`${linkText} ${getButtonStyle(
+                    false,
+                    writeCommentClicked
+                  )}`}
                 >
                   {writeCommentClicked ? "Cancel Comment" : "Write Comment"}
                 </button>
