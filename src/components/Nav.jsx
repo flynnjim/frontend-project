775870d5f5d4db352cd015312ebd953ec87ab39e
flynnjim <flynnlomas@hotmail.co.uk>
2025-02-04 14:39:54 +0000

    configured main navigation bar mobile first method

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,27 @@
 import { Link, useLocation } from "react-router-dom";
 
 const Nav = ({ topics }) => {
-
   const topicsArray = topics.map((topic) => topic.slug);
 
   const location = useLocation();
+  console.log(topicsArray);
 
   const linkText =
-    "px-4 py-2 text-lg font-semibold block min-h-auto min-w-auto text-sm sm:text-base md:text-2xl lg:text-3xl";
+    "px-2 py-2 text-fluid font-semibold block min-h-auto min-w-auto hover:border-b-4 hover:border-cardcolor cursor-pointer";
 
   const currentCategory = location.pathname.split("/").pop();
 
   const getButtonStyle = (buttonName) => {
-    if (topicsArray.includes(currentCategory) && buttonName === "articles") {
-      return "bg-fluorescent-yellow rounded-lg";
-    }
-    return currentCategory === buttonName
-      ? "bg-fluorescent-yellow rounded-lg"
-      : "bg-buttonpink rounded-lg";
+    // if (topicsArray.includes(currentCategory) && buttonName === "articles") {
+    //   return "border-b-4 border-cardcolor";
+    // }
+    return currentCategory === buttonName ? "border-b-4 border-cardcolor" : "";
   };
 
-
-
   return (
-    <header className="bg-bgcolor rounded-lg p-4 w-full ">
+    <header className="bg-bgcolor pt-2 w-full capitalize">
       <nav aria-label="Main Navigation">
-        <ul className="flex flex-wrap space-x-4 mx-auto justify-center w-full">
+        <ul className="flex flex-wrap mx-auto justify-start w-full divide-x-2 divide-black-100">
           <li className="transform hover:scale-105 transition-all">
             <Link to="/" className={`${linkText} ${getButtonStyle("")}`}>
               Home
@@ -41,21 +37,23 @@ const Nav = ({ topics }) => {
             </Link>
           </li>
           {topics.map((topic, index) => {
-            const uniqueKey = `topic-${index}`
+            const uniqueKey = `topic-${index}`;
             const topicName = topic.slug;
             return (
-              <li key={uniqueKey} className="transform hover:scale-105 transition-all">
+              <li
+                key={uniqueKey}
+                className="transform hover:scale-105 transition-all"
+              >
                 <Link
-              to={`/categories/${topicName}`}
-              className={`${linkText}
+                  to={`/categories/${topicName}`}
+                  className={`${linkText}
                  ${getButtonStyle(topicName)}`}
-            >
-              {topicName}
-            </Link>
+                >
+                  {topicName}
+                </Link>
               </li>
             );
           })}
-
         </ul>
       </nav>
     </header>
